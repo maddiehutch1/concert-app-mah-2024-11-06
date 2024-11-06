284a589b697a1668f00bb2dba34117afa310bd74
EC2 Default User <ec2-user@ip-10-0-14-171.us-east-2.compute.internal>
2024-11-06 21:02:09 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 let express = require("express");
 let app = new express();                                                                     
+app.set("view engine", "ejs");
 
 // set up database connection
 const knex = require("knex")({
  client: "mysql",
  connection: {
-  host:"concert-db-instance-1.c61dq6ysma4i.us-east-2.rds.amazonaws.com",
+  host:"concert-db.czkqq60wugm7.us-east-2.rds.amazonaws.com",
   user: "admin",
-  password: "Password1",
+  password: "Testing123",
   database:"paradise-concerts",
   port: 3306,
  },
@@ -17,9 +18,8 @@ app.get("/",(req,res) => {
  knex
  .select()
  .from("venues")
- .then((result) => {
-  console.log(result);
-  res.send(result);
+ .then((results) => {
+  res.render("index", {aConcerts: results});
  }); 
 });
 app.listen(3000);
